c45fdabdded68cf7bc7f24eb7c61bd7a9e521b32
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-04-02 22:22:48 +0200

    Add element for form width on 768px

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# set the DEBUG configuration option after creating the Flask app instance
+app.config['DEBUG'] = True
+
 @app.route('/')
 def index():
     return render_template("index.html")
