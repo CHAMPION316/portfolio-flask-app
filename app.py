3306fcd64b8cf13950a6ad89d1d97d5a44acbfbc
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-04-10 21:02:56 -0400

    Change path for nav-links

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home/index.html")
 
 @app.route('/contact')
 def contact():
@@ -20,7 +20,12 @@ def contact():
         return 'Thank you for your message'
     else:
         # Display the contact form
-        return render_template("contact/contact.html")
+        return render_template("home/contact.html")
+    
+@app.route('/bio')
+def biography():
+    return render_template("home/bio.html")
+
 
 if __name__ == "__main__":
     app.run()
